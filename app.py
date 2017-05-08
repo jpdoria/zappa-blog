0b7399c92559159c9e9a4593fa8df2c6392d63c6
jpdoria <jp@lazyadm.in>
2017-05-08 12:02:52 +0800

    Rewrite code

--- app.py
+++ app.py
@@ -1,70 +1,64 @@
-#!/usr/bin/env python
-
-from flask import Flask, request, render_template
-from jinja2 import Environment, FileSystemLoader
+import os
+import markdown2
 
 from dateutil.parser import parse
-from datetime import datetime, timedelta
-
-import markdown2
-import os
+from flask import Flask, render_template
+from jinja2 import Environment, FileSystemLoader
 
 app = Flask(__name__)
 
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 jenv = Environment(loader=FileSystemLoader(THIS_DIR), trim_blocks=True)
 
-@app.route('/', methods=['GET', 'POST'])
-def category(category="posts"):
+
+@app.route('/', methods=['GET'])
+def category(category='posts'):
     """
-    Categories of items.
-
-    Essentially a 'directory listing' page. 
-
-    Returns to the items to the 'index'.
-
+    This is the list of posts that are shown in '/'
     """
-
     try:
-
-        # First do the items
+        # Get the markdown files in 'posts' directory
         items = []
+
         for filename in os.listdir(category):
-            if 'index.md' in filename: 
+            if 'index.md' in filename:
                 continue
             if filename[-3:] == '.py':
                 continue
-            item = markdown2.markdown_path(category + '/' + filename, extras=["metadata"])
+
+            item = markdown2.markdown_path('{0}/{1}'.format(category,
+                                           filename), extras=['metadata'])
             item.metadata['slug'] = filename.split('/')[-1].replace('.md', '')
+
             items.append(item)
 
-        # Then do the index
+        # Display list of posts in '/'
         template = 'templates/category.html'
-
         render_data = {}
-        render_data[u'title'] = 'Zappatista! The Zappa Blog'
-        render_data[u'category'] = 'posts'
-        render_data[u'items'] = sorted(items, key=lambda item: parse(item.metadata.get('date_created', '')), reverse=True)
+        render_data[u'title'] = 'Jack of All Trades, Master of None'
+        render_data[u'category'] = category
+        render_data[u'items'] = sorted(items, key=lambda item: parse(
+                                       item.metadata.get('date_created', '')),
+                                       reverse=True)
 
         rendered = jenv.get_template(template).render(render_data)
-        return rendered
 
+        return rendered
     except IOError as e:
         print(e)
         return render_template('404.html'), 404
 
-@app.route('/<category>/<item_slug>', methods=['GET', 'POST'])
-def item(category, item_slug):
-    """
-    A single specific item.
 
+@app.route('/<item_slug>', methods=['GET'])
+def item(item_slug, category='posts'):
     """
-
+    Display a single post
+    """
     try:
-        path = category + '/' + item_slug + '.md'
-        html = markdown2.markdown_path(path, extras=["metadata"])
-
+        path = '{0}/{1}.md'.format(category, item_slug)
+        html = markdown2.markdown_path(path, extras=['metadata'])
         metadata = html.metadata
+
         if 'template' in metadata.keys():
             template = metadata['template']
         else:
@@ -74,14 +68,12 @@ def item(category, item_slug):
         render_data[u'body'] = html
         render_data[u'category'] = category
         render_data[u'item_slug'] = item_slug
-
         rendered = jenv.get_template(template).render(render_data)
-        return rendered
 
+        return rendered
     except IOError as e:
         return render_template('404.html'), 404
 
-# We only need this for local development.
+# For local development only
 if __name__ == '__main__':
-    app.debug = True
-    app.run()
+    app.run(debug=True, use_reloader=True)
